b1fe853f7e1066b312b6c5c1e0b6570bcbd2a8c3
Molisha-agarwal <molisha0102@gmail.com>
2025-02-21 21:57:53 +0530

    Time: 989 ms (6.23%), Space: 17.7 MB (83.33%) - LeetHub

---
+++ 0018-4sum/0018-4sum.py
@@ -0,0 +1,21 @@
+
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        n = len(nums)
+        st = set()
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                hashset = set()
+                for k in range(j + 1, n):
+                    fourth = target - (nums[i] + nums[j] + nums[k])
+                    if fourth in hashset:
+                        temp = [nums[i], nums[j], nums[k], fourth]
+                        temp.sort()  
+                        st.add(tuple(temp))  
+                    hashset.add(nums[k])
+        
+        return list(st) 
+             
+        
