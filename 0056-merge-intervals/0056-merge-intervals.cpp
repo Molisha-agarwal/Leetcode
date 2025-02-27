a25bdea488510c07023d305537ed83b737fa2e19
Molisha-agarwal <molisha0102@gmail.com>
2025-02-27 19:28:39 +0530

    Time: 3 ms (89.76%), Space: 23.8 MB (79.87%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-      int n=intervals.size();
-      sort(intervals.begin(),intervals.end());
-       vector<vector<int>>ans;
-       for(int i=0;i<n;i++){
-
-        if(ans.empty() || intervals[i][0]>ans.back()[1]){
-            ans.push_back(intervals[i]);
+        vector<vector<int>>ans; //to store the elements as answer 
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        for(int i=0;i<n;i++){
+            //check if answer array is empty so that new element is pushed and also chack for the non overlapping intervals
+           if(ans.empty()||ans.back()[1]<intervals[i][0])
+              ans.push_back(intervals[i]);
+           else
+           //interval ko expand karna padega naa to fit the elements
+               ans.back()[1] =max(ans.back()[1],intervals[i][1]); 
         }
-        else{
-            ans.back()[1]=max(ans.back()[1],intervals[i][1]);
-        }
-       }  
-       return ans;
+         return ans;  
     }
-
 };
